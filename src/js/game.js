be9dd0640d6796b73ebdbfd0a8a55284d5333dde
Mostafa Salama <mostafatourad789@gmail.com>
2020-09-25 01:31:21 +0200

    create class boared to store the info about each grid item as a box

--- src/js/game.js
+++ src/js/game.js
@@ -16,3 +16,20 @@ class ItemPosition {
 		this.y = y;
 	}
 }
+class BoardBox {
+	/**
+	 *
+	 * @param position {ItemPosition}
+	 * @param element {HTMLElement}
+	 * @param index {number}
+	 */
+	constructor(position, element, index) {
+		this.position = position;
+		this.element = element;
+		this.index = index;
+		this.isEmpty = true;
+		this.filledWith = null;
+		this.weapon = null ;
+		this.player = null ;
+	}
+}
