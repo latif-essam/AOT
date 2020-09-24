24ea285f3902cf67b44570c5b2af906885743729
Mostafa Salama <mostafatourad789@gmail.com>
2020-09-25 01:29:40 +0200

    add class ItemPosition to store info about each item in the grid map

--- src/js/game.js
+++ src/js/game.js
@@ -2,3 +2,17 @@
 const player1 = document.getElementById("health1");
 const player2 = document.getElementById("health2");
 
+/**
+ * store the position of an item using its x and y
+ */
+class ItemPosition {
+	/**
+	 *
+	 * @param x {number} x-axis position (row)
+	 * @param y {number} y-axis position (column)
+	 */
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+}
